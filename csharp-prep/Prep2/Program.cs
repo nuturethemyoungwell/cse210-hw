adb79872151f8b72b4f4adc3e5c8a8752737f2c5
raphaelprominence <raphaelprominence10@gmail.com>
2023-04-24 20:30:27 +0100

    prep2 commit

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -3,7 +3,56 @@ using System;
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep2 World!");
+     {
+        Console.Write("What is your grade percentage? ");
+        string grade = Console.ReadLine();
+        int gradepercent = int.Parse(grade);
+
+        string letter = "";
+        
+
+        if (gradepercent >= 97)
+        {
+            letter = "A+";
+        }
+        else if (gradepercent >= 90)
+        {
+            letter = "A";
+        }
+
+        else if (gradepercent >= 80)
+        {
+            letter = "B";
+        }
+
+        else if (gradepercent >= 70)
+        {
+            letter = "C";
+        }
+        else if (gradepercent >= 60)
+        {
+            letter = "D";
+        }
+
+        
+
+        else
+        {
+            letter = "F";
+        }
+
+    
+        Console.WriteLine($"congrat your grade is {letter}");
+
+        if (gradepercent >= 70)
+        {
+            Console.WriteLine("You passed!");
+        }
+
+        else
+        {
+            Console.WriteLine("Better luck next time!");
+        }
+        
     }
 }
